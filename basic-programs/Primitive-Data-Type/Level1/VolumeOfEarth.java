9af03bb612e9255de9fa1350ee1451c7051e7c6a
princekumar-01 <princekumargithub01@gmail.com>
2026-01-19 23:51:20 +0530

    Lvl 1 Problem Solutions

--- basic-programs/Primitive-Data-Type/Level1/VolumeOfEarth.java
+++ basic-programs/Primitive-Data-Type/Level1/VolumeOfEarth.java
@@ -3,8 +3,8 @@ public class VolumeOfEarth {
         double radius = 6378;
         double pi = 3.14159;
         
-        double volumeKm = (4.0 / 3.0) * pi * radius * radius * radius;
-        double volumeMiles = volumeKm / (1.609 * 1.609 * 1.609);
+         double volumeKm = (4.0 / 3.0) * pi * radius * radius * radius;
+         double volumeMiles = volumeKm / (1.609 * 1.609 * 1.609);
         
         System.out.println("The volume of earth in cubic kilometers is " + volumeKm + " and cubic miles is " + volumeMiles);
     }
