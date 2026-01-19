a11967f7d86c68f30bf05787c739a8ff9dfe2dc8
princekumar-01 <princekumargithub01@gmail.com>
2026-01-19 23:51:47 +0530

    Lvl 2 Problem Solutions

--- basic-programs/Primitive-Data-Type/Level2/AreaOfTriangleLevel2.java
+++ basic-programs/Primitive-Data-Type/Level2/AreaOfTriangleLevel2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class AreaOfTriangleLevel2 {
+    public class AreaOfTriangleLevel2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
